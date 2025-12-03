35210f74a6607384cece1411c54081fc11461724
Ravi <ravindramore6@gmail.com>
2025-12-03 15:30:04 +0530

    Basic code checkin day1 and day2

---
+++ basic/day-1/js/app.js
@@ -0,0 +1,54 @@
+ 
+
+/***
+ * 
+ *  <div id="parent">
+ *     <div id="child">
+ *            <h1> I am h1 tag</h1>
+ *            <h2> I am h1 tag</h2>
+ *     <div>
+ * 
+ * <div>
+ * 
+ * const h2 = React.createElement("h2", {}, "This is h2 tag");
+const h3 = React.createElement("h3", {}, "This is h3 tag");
+const child = React.createElement("div", {id:"child1"}, [h2, h3]);
+
+ */
+
+
+ 
+
+/***
+ * 
+ *  <div id="parent">
+ *     <div id="child1">
+ *            <h1> I am h1 tag</h1>
+ *            <h2> I am h1 tag</h2>
+ *     <div>
+ * <div id="child2">
+ *            <h1> I am h1 tag</h1>
+ *            <h2> I am h1 tag</h2>
+ *     <div>
+ * 
+ * <div>
+ */
+
+const h2ch1 = React.createElement("h2", {}, "This is child 1 h2 tag");
+const h3ch1 = React.createElement("h3", {}, "This ischild 1  h3 tag");
+const child1 = React.createElement("div", {id:"child1"}, [h2ch1, h3ch1]);
+
+const h2ch2 = React.createElement("h2", {}, "This is h2 tag");
+const h3ch2 = React.createElement("h3", {}, "This is h3 tag");
+const child2 = React.createElement("div", {id:"child1"}, [h2ch2, h3ch2]);
+
+
+const parent = React.createElement("div", {id:"paranet"},[child1, child2] );
+
+ 
+//const heading = React.createElement("h1", {id:"heading"}, "Hello from React");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
+        
