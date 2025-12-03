2c7ef7646ec70ac7c476f76f6e1dd683ad478fa5
Ravi <ravindramore6@gmail.com>
2025-12-03 23:14:46 +0530

    Added day 3 information

--- basic/day-3-react-parcel-prod/app.js
+++ basic/day-3-react-parcel-prod/app.js
@@ -1,4 +1,5 @@
- 
+ import React from "react";
+ import ReactDOM from "react-dom/client";
 
 /***
  * 
@@ -34,13 +35,14 @@ const child = React.createElement("div", {id:"child1"}, [h2, h3]);
  * <div>
  */
 
-const h2ch1 = React.createElement("h2", {}, "This is child 1 h2 tag from react");
-const h3ch1 = React.createElement("h3", {}, "This ischild 1  h3 tag");
-const child1 = React.createElement("div", {id:"child1"}, [h2ch1, h3ch1]);
 
-const h2ch2 = React.createElement("h2", {}, "This is h2 tag");
-const h3ch2 = React.createElement("h3", {}, "This is h3 tag, I am from react and load faster.");
-const child2 = React.createElement("div", {id:"child1"}, [h2ch2, h3ch2]);
+const h2ch1 = React.createElement("h2", {id:"h2ch1", key:"h2ch1"}, "This is child 1 h2 tag from react");
+const h3ch1 = React.createElement("h3", {id:"h3ch1", key:"h3ch1"}, "This ischild 1  h3 tag");
+const child1 = React.createElement("div", {id:"child1", key:"C1"}, [h2ch1, h3ch1]);
+
+const h2ch2 = React.createElement("h2", {id:"h2ch2", key: "h2ch2"}, "This is h2 tag");
+const h3ch2 = React.createElement("h3", {id:"h3ch2", key:"h3ch2"}, "This is h3 tag, I am from react and load faster.");
+const child2 = React.createElement("div", {id:"child2", key:"C2"}, [h2ch2, h3ch2]);
 
 
 const parent = React.createElement("div", {id:"paranet"},[child1, child2] );
@@ -51,4 +53,6 @@ const parent = React.createElement("div", {id:"paranet"},[child1, child2] );
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(parent);
+
+// Notes: 
         
